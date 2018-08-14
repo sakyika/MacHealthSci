35019998c06e4abe5d65a2699e4a72b9fabc992c
Kwadwo Sakyi <kwadwosakyi@gmail.com>
2018-08-14 13:59:00 -0400

    added ! to message

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (requestAnimationFrame, res) => {
-    res.send({ message: 'Welcome McMaster Health Scieces'});
+    res.send({ message: 'Welcome McMaster Health Scieces!'});
 });
 
 const PORT = process.env.PORT || 5000;
