3e5eb7e4621b6d3f0c807f259e8b849a618fd04a
Kwadwo Sakyi <kwadwosakyi@gmail.com>
2018-08-14 14:45:29 -0400

    updating project setup

---
+++ server/config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoUri: 'mongodb://sakksoftware:sakk11software@ds121282.mlab.com:21282/machealthsci-dev'
+};
--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
 const app = express();
 
+app.use(morgan('combined'));
+// app.use(bodyParser.json({ type: '*/*' }));
+
+require('./routes/authRoutes')(app);
+
 app.get('/', (requestAnimationFrame, res) => {
-    res.send({ message: 'Welcome McMaster Health Scieces!'});
+    res.send({ message: 'Welcome McMaster Health Sciences!'});
 });
 
 const PORT = process.env.PORT || 5000;
