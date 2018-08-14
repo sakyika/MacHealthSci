9e053a2f4dffc4e167d750699a61cca5948fdf31
Kwadwo Sakyi <kwadwosakyi@gmail.com>
2018-08-14 13:50:36 -0400

    initial commit

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (requestAnimationFrame, res) => {
+    res.send({ message: 'Welcome McMaster Health Scieces'});
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
