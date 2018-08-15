5500013d406735fb4b56f580b5575eab3d09902f
Kwadwo Sakyi <kwadwosakyi@gmail.com>
2018-08-15 09:36:42 -0400

    fixing heroku issues

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,7 +4,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Welcome to McMaster Health Sciences</h1>
+        <h1>Welcome to McMaster Health Sciences!</h1>
       </div>
     );
   }
